02c7f5a8aa52105a6d9738efefe92d514ad97808
Brian Ailinger <brianjr48@yahoo.com>
2020-04-19 20:46:05 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -12,4 +12,17 @@ end
 
 sort_array_desc([25, 7, 14])
 
+def sort_array_char_count(array)
+  array.sort do |left, right|
+    left.length <=> right.length
+  end
+end
 
+sort_array_char_count(["dogs", "cat", "Horses"]) 
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+swap_elements(["blake", "ashley", "scott"])
